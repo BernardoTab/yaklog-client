cd086e678354563b839614db7c880201539e6a9a
BernardoTab <bernardotab@gmail.com>
2025-09-28 18:21:26 +0100

    fix: flicker fix

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,27 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from '../../features/auth/services/auth.service';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    @Inject(PLATFORM_ID)private platformId: Object) { }
 
-  canActivate(): Observable<boolean> {
+  canActivate(): Observable<boolean | UrlTree> {
+    if(isPlatformServer(this.platformId)){
+      return of(false);
+    }
     return this.authService.preloadCurrentUser().pipe(
       map(user => {
         if (!user) {
-          this.router.navigate(['/login']);
-          return false;
+          return this.router.createUrlTree(['/login']);
         }
         return true;
       }),
       catchError(() => {
-        this.router.navigate(['/login']);
-        return of(false);
+          return of(this.router.createUrlTree(['/login']));
       })
     );
   }
